1cf8514e9337ff526e96366381949934566bb364
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2024-12-18 21:27:56 +0400

    fix(debounce): types

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,7 +1,11 @@
-export function debounce(func: (...args: any[]) => void, delay: number) {
-  let timeout: NodeJS.Timeout;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+) {
+  let timeout: ReturnType<typeof setTimeout>; // Correct timeout type for browser and Node.js
 
-  const debounced = (...args: any[]) => {
+  const debounced = (...args: Parameters<T>) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), delay);
   };
