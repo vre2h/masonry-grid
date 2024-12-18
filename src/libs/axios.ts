9f60257009535dc54bf844f4a1dc28d00074da3e
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2024-12-18 20:07:10 +0400

    fix(axios): update .env var names

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.PEXELS_KEY;
-const BASE_URL = import.meta.env.PEXELS_BASE_URL;
+const API_KEY = import.meta.env.VITE_PEXELS_KEY;
+const BASE_URL = import.meta.env.VITE_PEXELS_BASE_URL;
 
 export const axiosClient = axios.create({
   baseURL: BASE_URL,
